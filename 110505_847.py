a546774b6d24b040ec7873e51a85119e5f339a07
Leonardo YongUk Kim <dalinaum@gmail.com>
2012-08-15 17:49:34 +0900

    add 110505_847.

---
+++ 110505_847.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+import sys
+import math
+
+n = int(raw_input())
+
+while 1:
+    n = int(math.ceil(n / 9.0));
+    if n == 1:
+        print "Stan wins."
+        break
+    n = int(math.ceil(n / 2.0));
+    if n == 1:
+        print "Ollie wins."
+        break
